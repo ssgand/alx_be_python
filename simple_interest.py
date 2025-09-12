286f5d4b4c421b2f5f84d241f7e4ca45a66a3fad
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-09-12 23:36:58 +0200

    Added a new file containing a simple interest calculation

---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # represents the initial investment of $1000
+rate = 0.05  # represents 5% annual interest rate
+time = 3  # represents the time the money is invested for in years, 3 years in this case
+
+interest = principal * rate * time
+
+print('The simple interest is: ' + str(interest))
