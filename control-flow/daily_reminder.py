b6019544399977b05fb8df06f59de6926f9ccbdf
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-09-21 23:58:07 +0200

    Added a file that simulates a to do list

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+match priority:
+	case "high":
+		if time_bound:
+			print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+		else:
+			print(f"Note: '{task}' is a high priority task. Consider doing it as soon as you can!")
+	case "medium":
+		if time_bound:
+			print(f"Reminder'{task}' is a medium priority task but is time-bound. Consider completing it as soon as you have time!")
+		else:
+			print(f"Note: '{task}' is a medium priority task. Consider doing it later or delegating it!")
+	case "low":
+		if time_bound:
+			print(f"Reminder: '{task}' is a low priority task but is time-bound. Consider delegating it or completing it as soon as you can!")
+		else:
+			print(f"Note: '{task}' is a low priority task, Consider completing it when you have free time.")
