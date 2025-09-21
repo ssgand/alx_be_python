afb83fcc0f35cebbada7a8c9b83e75ef5054482d
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-09-21 21:38:08 +0200

    Minor modifications

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for num in range(1, 11):
-	print(str(number) + " * " + str(num) + " = " + str(number * num))
+	print(f"{number} * {num} = {number * num}")
