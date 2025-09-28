787c49cd9947e68400f5a664d1eb984d298208c5
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-09-28 18:31:05 +0200

    Minor modifications

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ def perform_operation(num1, num2, operation):
 	match(operation):
 		case "add":
 			return num1 + num2
-		case "substract":
+		case "subtract":
 			return num1 - num2
 		case "multiply":
 			return num1 * num2
