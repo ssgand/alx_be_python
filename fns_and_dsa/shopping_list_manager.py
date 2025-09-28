c1bf7ef95ea258a28ef458b5a263a55a7238605a
Sekamondo Steve Gandhi <steve.gandhi@wiredin.rw>
2025-09-28 20:27:39 +0200

    Added a script to add/remove items from a shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+def display_menu():
+	print("Shopping List Manager")
+	print("1. Add Item")
+	print("2. Remove Item")
+	print("3. View List")
+	print("4. Exit")
+
+def main():
+	shopping_list = []
+	option = 0
+	while option != 4:
+		display_menu()
+		choice = input("Enter your choice: ")
+
+		if choice == '1':
+			# Prompt for and add an item
+			item = input("Enter the item to add: ")
+			shopping_list.append(item)
+		elif choice == '2':
+			# Prompt for and remove an item
+			item = input("Enter the item to remove: ")
+			if item in shopping_list:
+				shopping_list.remove(item)
+			else:
+				print(f"{item} not in shopping list")
+		elif choice == '3':
+			# Display the shopping list
+			for item in shopping_list:
+				print(item)
+		elif choice == '4':
+			print("Goodbye!")
+			break
+		else:
+			print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
